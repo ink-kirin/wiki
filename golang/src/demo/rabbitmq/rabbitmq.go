1736d59fd0af562e5a86c20e97521192f7532521
lizheng <923147568@qq.com>
2021-04-22 09:55:03 +0800

    gitignore

--- golang/src/demo/rabbitmq/rabbitmq.go
+++ golang/src/demo/rabbitmq/rabbitmq.go
@@ -33,9 +33,22 @@ func (r *RabbitMQ) Destory() {
 	_ = r.conn.Close()
 }
 
-func (r *RabbitMQ) failOnError(err error, message string) {
+// 错误处理函数
+func (r *RabbitMQ) failOnErr(err error, message string) {
 	if err != nil {
 		log.Fatalf("%s:%s", message, err)
-		panic(fmt.Sprintf("%s:%s"), message, err)
+		panic(fmt.Sprintf("%s:%s", message, err))
 	}
+}
+
+//简单模式Step 1.创建简单模式下的RabbitMq实例
+func NewRabbitMQSimple(queueName string) *RabbitMQ {
+	rabbitmq := NewRabbitMQ(queueName, "", "")
+	var err error
+	// 创建rabbitmq链接
+	rabbitmq.conn, err = amqp.Dial(rabbitmq.Mqurl)
+	rabbitmq.failOnErr(err, "创建链接错误")
+	rabbitmq.channel, err = rabbitmq.conn.Channel()
+	rabbitmq.failOnErr(err, "获取channel失败")
+	return rabbitmq
 }
